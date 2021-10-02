1354634909ccde51bd1ca0b46e28f6f30cf2b47c
OmarMWarraich <earthling986@gmail.com>
2021-10-02 07:33:52 -0400

    Add Dependency and Console.log Account Balance

--- app.js
+++ app.js
@@ -2,5 +2,14 @@ const Web3 = require('web3')
 const rpcURL = 'https://mainnet.infura.io/v3/fca07a6fa2974cc39b20c23eff5a323a' 
 const web3 = new Web3(rpcURL)
 const address = '0x00000000219ab540356cBB839Cbe05303d7705Fa' 
-web3.eth.getBalance(address, (err, wei) => { balance = web3.utils.fromWei(wei, 'ether') })
+// web3.eth.getBalance(address, (err, wei) => { balance = web3.utils.fromWei(wei, 'ether') })
 
+web3.eth.getBalance(address, function(error,result){
+
+    if(error){
+       console.log(error)
+    }
+    else{
+       console.log(result)
+    }
+ })
