962a6dbbc92ab23a85a145c9e7985957e4e22e0d
OmarMWarraich <earthling986@gmail.com>
2021-10-07 17:43:14 -0400

    Smart Contract Events with Web3.js

--- app.js
+++ app.js
@@ -1,49 +1,21 @@
-var Tx = require('ethereumjs-tx').Transaction
+// var Tx = require('ethereumjs-tx').Transaction
 const Web3 = require('web3')
 
-const web3 = new Web3('https://ropsten.infura.io/v3/e989a4425571442fb0b852fe100ea2a4')
+const web3 = new Web3('https://mainnet.infura.io/v3/fca07a6fa2974cc39b20c23eff5a323a')
 
-const account1 = '0x083DA9aB36d05b7045688eA076CF3480De15aFF3';
-const account2 = '0x6d99b45Df81DBe791Fd484F52AFCdb8B2Dff47d3';
+// OMG Token Contract
+const abi = [{"constant":true,"inputs":[],"name":"mintingFinished","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"finishMinting","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"},{"name":"_releaseTime","type":"uint256"}],"name":"mintTimelocked","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[],"name":"MintFinished","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]
+const address = '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07'
 
-const k1 = Buffer.from(process.env.PRIVATE_KEY_1, 'hex');
-const k2 = Buffer.from(process.env.PRIVATE_KEY_2, 'hex');
+const contract = new web3.eth.Contract(abi, address)
 
-// Read the deployed contract - get the addresss from Etherscan
-const contractAddress = '0xd03696B53924972b9903eB17Ac5033928Be7D3Bc'
-const abi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"standard","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"}]
+// Get Contract Event Stream
+contract.getPastEvents(
+  'allEvents',
+  {
+    fromBlock: 5854000,
+    toBlock: 'latest'
+  },
+  (err, events) => { console.log(events) }
+)
 
-const contract = new web3.eth.Contract(abi, contractAddress)
-
-// Transfer some tokens
-web3.eth.getTransactionCount(account1, (err, txCount) => {
-
-    const txObject = {
-      nonce:    web3.utils.toHex(txCount),
-      gasLimit: web3.utils.toHex(800000), // Raise the gas limit to a much higher amount
-      gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
-      to: contractAddress,
-      data: contract.methods.transfer(account2, 1).encodeABI()
-    }
-  
-    const tx = new Tx(txObject, {'chain':'ropsten'});
-    tx.sign(k1)
-  
-    const serializedTx = tx.serialize()
-    const raw = '0x' + serializedTx.toString('hex')
-  
-    web3.eth.sendSignedTransaction(raw, (err, txHash) => {
-      console.log('err:', err, 'txHash:', txHash)
-      // Use this txHash to find the contract on Etherscan!
-    })
-  })
-  
-  // Check Token balance for account1
-  contract.methods.balanceOf(account1).call((err, balance) => {
-    console.log({ err, balance })
-  })
-  
-  // Check Token balance for account2
-  contract.methods.balanceOf(account2).call((err, balance) => {
-    console.log({ err, balance })
-  })
