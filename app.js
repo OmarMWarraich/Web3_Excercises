801819a43cc758b73f3de74662a84db716051304
OmarMWarraich <earthling986@gmail.com>
2021-10-07 18:09:53 -0400

    Use utilities

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ const Web3 = require('web3')
 
 const web3 = new Web3('https://mainnet.infura.io/v3/fca07a6fa2974cc39b20c23eff5a323a')
 
-// // get latest block number
-// web3.eth.getBlockNumber().then(console.log)
-
-// get latest block
-// web3.eth.getBlock('latest').then(console.log)
-
-// // get latest 10 blocks
-// web3.eth.getBlockNumber().then((latest) => {
-//   for (let i = 0; i < 10; i++) {
-//     web3.eth.getBlock(latest - i).then(console.log)
-//   }
+// Get average gas price in wei from last few blocks median gas price
+// web3.eth.getGasPrice().then((result) => {
+//   console.log(web3.utils.fromWei(result, 'ether'))
 // })
 
-// get transaction from specific block
-const hash = '0x063e7345937b72be501e6c4b3a5504c13b373911a19e9c9d547edbfac431c5ec'
-web3.eth.getTransactionFromBlock(hash, 13374326).then(console.log)
+// // Use sha256 Hashing function
+// console.log(web3.utils.sha3('Dapp University'))
 
+// // Use keccak256 Hashing function (alias)
+// console.log(web3.utils.keccak256('Dapp University'))
 
+// Get a Random Hex
+// console.log(web3.utils.randomHex(32))
 
+// Get access to the underscore JS library
+const _ = web3.utils._
 
+_.each({ key1: 'value1', key2: 'value2' }, (value, key) => {
+  console.log(key)
+})
